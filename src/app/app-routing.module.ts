4ec286c82eccb92e1a573a083f13300acb36dd1c
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-04-10 13:37:49 +0200

    added route to dataTransferComp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataTransferCollectionComponent } from './components/data-transfer-between-components/data-transfer-collection.component';
 
-const routes: Routes = [];
+/* add the component to the route path name is creative */
+const routes: Routes = [
+  {path: 'dataTransferCollection', component: DataTransferCollectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
