d55e1512aae17574ecb5ae3b065ef4e2e69ac3dd
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-04-22 11:51:14 +0200

    added routing to table component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataTransferCollectionComponent } from './components/data-transfer-between-components/data-transfer-collection.component';
+import { TableCollectionComponent } from './components/using-tables/table-collection.component';
 
 /* add the component to the route path name is creative */
 const routes: Routes = [
-  {path: 'dataTransferCollection', component: DataTransferCollectionComponent}
+  {path: 'dataTransferCollection', component: DataTransferCollectionComponent},
+  {path: 'tableCollection', component: TableCollectionComponent}
 ];
 
 @NgModule({
